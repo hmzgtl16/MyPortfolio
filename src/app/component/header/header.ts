cf93b56bbf7875a5025a15a0903d626d38a1afe8
hmzgt <hmzgtl16@gmail.com>
2025-08-18 00:05:39 +0100

    update styles and structure for header and home components

--- src/app/component/header/header.ts
+++ src/app/component/header/header.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faBars, faCircleHalfStroke, faMoon } from '@fortawesome/free-solid-svg-icons'
 import { faSun } from '@fortawesome/free-regular-svg-icons';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [ FontAwesomeModule ],
+  imports: [FontAwesomeModule, NgClass],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
