5fdbc2d26203304c157a381d06354230cb8e3f96
hmzgt <hmzgtl16@gmail.com>
2025-08-18 15:54:25 +0100

    add services component with template, styles, and tests

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Header } from './component/header/header';
 import { Home } from './component/home/home';
 import { About } from './component/about/about';
+import {Services} from './component/services/services';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Home, About],
+  imports: [Header, Home, About, Services],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
