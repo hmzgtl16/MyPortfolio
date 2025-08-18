3f0ad8c4ab45221af2db364c1ae7a0732b6ac0d7
hmzgt <hmzgtl16@gmail.com>
2025-08-18 13:31:01 +0100

    add about component with content and styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from './component/header/header';
 import { Home } from './component/home/home';
+import { About } from './component/about/about';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Home],
+  imports: [Header, Home, About],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
